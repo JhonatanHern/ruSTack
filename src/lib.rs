9e95a039f0e6001759037fbb56b8f98a895a24d1
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-04-08 17:35:44 -0400

    test optimized

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,9 @@
 
 pub mod stack;
 
-// use first::Stack;
-
 #[cfg(test)]
 mod tests {
 	use crate::stack::Stack;
-    fn get_value_or_0(arg: Option<i32>) -> i32 {
-        match arg {
-            Some(num) => num,
-            None => -1,
-        }
-    }
-    fn get_value_or_empty_str(arg: Option<String>) -> String {
-        match arg {
-            Some(stri) => stri,
-            None => String::from(""),
-        }
-    }
     #[test]
     fn push_n_pop_int() {
         let mut s = Stack::new();
@@ -26,9 +12,9 @@ mod tests {
         s.push( 1 );
         s.push( 7 );
         assert_eq!( 3 , s.length() );
-        assert_eq!( 7 , get_value_or_0(s.pop()) );
-        assert_eq!( 1 , get_value_or_0(s.pop()) );
-        assert_eq!( 17 , get_value_or_0(s.pop()) );
+        assert_eq!( 7 , s.pop().unwrap() );
+        assert_eq!( 1 , s.pop().unwrap() );
+        assert_eq!( 17 , s.pop().unwrap() );
         assert_eq!( 0 , s.length() );
         assert_eq!( None , s.pop() );
         assert_eq!( 0 , s.length() );
@@ -41,9 +27,9 @@ mod tests {
         s.push( String::from("1") );
         s.push( String::from("7") );
         assert_eq!( 3 , s.length() );
-        assert_eq!( String::from("7") , get_value_or_empty_str(s.pop()) );
-        assert_eq!( String::from("1") , get_value_or_empty_str(s.pop()) );
-        assert_eq!( String::from("17") , get_value_or_empty_str(s.pop()) );
+        assert_eq!( String::from("7") , s.pop().unwrap() );
+        assert_eq!( String::from("1") , s.pop().unwrap() );
+        assert_eq!( String::from("17") , s.pop().unwrap() );
         assert_eq!( 0 , s.length() );
         assert_eq!( None , s.pop() );
         assert_eq!( 0 , s.length() );
